776a2906198e68f36bfd41804be4c6f80cd6d408
Sha211 <48889286+Sha211@users.noreply.github.com>
2019-04-11 12:11:11 -0400

    Update week3_assignment.R
    I have added comments to the code to explain the steps

--- week3_assignment.R
+++ week3_assignment.R
@@ -1,11 +1,16 @@
 makeCacheMatrix <- function(x = matrix()) {
   im <- NULL
+  ##setting the value of the matrix
   set <- function(y) {
     x <<- y
+    ##this <<- operator is to assign value in an environment outside the parent
     im <<- NULL
   }
+  ##get the value of the matrix
   get <- function() x
+  ## set the value of inverse of matrix
   setinverse <- function(inverse) im <<- inverse
+  ## here we get the inverse of the matrix
   getinverse <- function() im
   list(set = set,
        get = get,
@@ -14,14 +19,22 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 cacheSolve <- function(x, ...) {
+  ##output of the makeCacheMatrix function
+  ## if the ivnerse has already existed in cache
   im <- x$getinverse()
   if(!is.null(im)) {
+    ##get it from cache and skip computation
     message("getting cached data")
     return(im)
   }
   data <- x$get()
-  ##solve returns the inverse of the matrix
+  ##otherwise solve returns the inverse of the matrix
   im <- solve(data, ...)
   x$setinverse(im)
   im
 }
+##example run
+x<- matrix(1:4,2,2)
+x
+x1<-makeCacheMatrix(x)
+cacheSolve(x1)
